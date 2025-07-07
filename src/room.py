f3f05c8c623e7564314e46571240a27f9a9b306e
Sigma <maddoxaherin@gmail.com>
2025-07-07 19:00:27 +0100

    Fleshed out room.py and Room class

--- src/room.py
+++ src/room.py
@@ -1,9 +1,21 @@
+# room class
 
-class room:
-    self.me =
-
-
-    # Constructor
-    def __init__(self,roomID):
+class Room:
+    def __init__(self,roomID, name : str, description : str):
         self.roomID = roomID
+        self.name = name
+        self.description = description
+        self.neighbors = []
+        self.occupants = []
 
+    def get_ID(self):
+        return self.roomID
+    def get_name(self):
+        return self.name
+    def get_description(self):
+        return self.description
+    def get_neighbors(self):
+        return self.neighbors
+    def get_occupants(self):
+        return self.occupants
+    
