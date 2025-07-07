d550c63503d794b36d03ce7ef4c40242375127f2
Sigma <maddoxaherin@gmail.com>
2025-07-07 21:13:46 +0100

    room info, puts info into a dictionary

--- src/main.py
+++ src/main.py
@@ -4,14 +4,31 @@
 #imports
 from room import Room
 from player import Player
+from actor import Actor
 
 def main():
-    print("AAAAAAGGGGHHHHHHHH!")
-    medbay = Room(1,"Medbay","There's like medical equipment and stuff")
-    shredder = Room(2,"Shredder","This is where failed paitients go to get better")
-    medbay.connect(shredder)
-    print(shredder.neighbors)
-    print(medbay.neighbors)
+
+    # Declare initial rooms
+    roomInfo = [
+        [0, "Bridge", ""]
+        [1, "North Hallway", ""]
+        [2, "South Hallway", ""]
+        [3, "Cryobay", ""]
+        [4, "Mess Hall", ""]
+        [5, "Medbay", ""]
+        [6, "North Engine", ""]
+        [7, "South Engine", ""]
+        [8, "Electrical Room", ""]
+        [9, "Laboratory", ""]
+        [10, "Dormitory", ""]
+        [11, "Security Office", ""]
+        [12, "Cargo Hold", ""]
+    ]
+
+    
+    for room_id, name, description in roomInfo:
+        room = Room(room_id, name, description)
+        rooms[name] = room
 
 
 if __name__ == "__main__":
