1ba95e6b706aad399c5bfb9696f95e4f8bf215d3
RAALightningn <ronanaherin@gmail.com>
2025-07-07 19:55:52 +0100

    added dead and threat to player

--- src/player.py
+++ src/player.py
@@ -1,6 +1,24 @@
 
-class player:
+class Player:
 
     def __init__(self,dead,threat):
-        self.dead = false;
-        self.threat = 0;
+        self.dead = false
+        self.threat = 0
+
+    def get_threat(self):
+        return self.threat
+
+    def is_dead(self):
+        return self.dead
+
+    def inc_threat(self):
+        self.threat += 1
+
+    def dec_threat(self):
+        self.threat -= 1
+
+    def set_threat(self, threat: int)
+        self.threat = threat
+
+    def kill(self):
+        self.dead = true
